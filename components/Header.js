f5bd70961330d35e8962a2cf9d797b7a14869b3e
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2017-06-23 16:19:56 +0800

    加入mobx

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+import { Navbar } from 'react-bootstrap'
+
+const navbarInstance = (
+    <Navbar>
+        <Navbar.Header>
+            <Navbar.Brand>
+                <a href="#">HP Predict</a>
+            </Navbar.Brand>
+        </Navbar.Header>
+    </Navbar>
+)
+
+export default () => (navbarInstance)
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Button } from 'react-bootstrap'
 import Page from '../layouts/main'
+import Header from '../components/Header.js'
 
 export default () => (
   <Page>
-    <Button bsStyle='primary'>testing</Button>
+    <Header />
   </Page>
 )
