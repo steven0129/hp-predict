543cae47b3f5b2eb831a9d93715ec0c949b7f22a
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2017-06-23 14:15:53 +0800

    init commit

---
+++ layouts/main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default ({ children }) => (
+    <div >
+        <Head>
+            <title>HP Predict</title>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css' />
+            <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap-theme.min.css' />
+        </Head>
+        {children}
+    </div >
+)
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+import Page from '../layouts/main'
+
+export default () => (
+  <Page>
+    <Button bsStyle='primary'>testing</Button>
+  </Page>
+)
