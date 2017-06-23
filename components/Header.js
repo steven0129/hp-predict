d16156683984883d7ce7baa41de0aae3db05083e
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2017-06-23 22:48:52 +0800

    mobx架構組成

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,30 @@
-import { Navbar } from 'react-bootstrap'
+import React, { Component } from 'react'
+import { Nav, Navbar, NavItem } from 'react-bootstrap'
+import { observer } from 'mobx-react'
 
-const navbarInstance = (
-    <Navbar>
-        <Navbar.Header>
-            <Navbar.Brand>
-                <a href="#">HP Predict</a>
-            </Navbar.Brand>
-        </Navbar.Header>
-    </Navbar>
-)
+@observer
+class Header extends Component {
+    render() {
+        return (
+            <Navbar inverse={this.props.state.nav.inverse} collapseOnSelect>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <a href="#">{this.props.state.nav.navHeader}</a>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav>
+                        {
+                            this.props.state.nav.navItems.map((value, index) => {
+                                return <NavItem key={index}>{value}</NavItem>
+                            })
+                        }
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        )
+    }
+}
 
-export default () => (navbarInstance)
+export default Header
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,30 @@
 import React from 'react'
-import { Button } from 'react-bootstrap'
+import { Grid, Row, Col } from 'react-bootstrap'
+import { observable } from 'mobx'
 import Page from '../layouts/main'
 import Header from '../components/Header.js'
+import Jumbotrn from '../components/Jumbotron.js'
+
+var state = observable({
+  nav: {
+    inverse: true,
+    navHeader: 'HP predict',
+    navItems: [
+      '首頁', '房市現況'
+    ]
+  }
+})
+
 
 export default () => (
   <Page>
-    <Header />
+
+    <Header state={state} />
+
+    <Grid>
+      <Row className="show-grid">
+        <Col xs={24} md={24}><Jumbotrn state={state} /></Col>
+      </Row>
+    </Grid>
   </Page>
 )
