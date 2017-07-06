1266b3b5dd503444965e0b0e8813a2fa792e6c6e
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2017-07-06 17:46:34 +0800

    add react-gmaps

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,13 @@ import { Grid, Row, Col } from 'react-bootstrap'
 import { observable } from 'mobx'
 import Page from '../layouts/main'
 import Header from '../components/Header.js'
+import Map from '../components/Map.js'
 
 export default () => (
   < Page >
 
     <Header />
+    <Map />
 
   </Page >
 )
